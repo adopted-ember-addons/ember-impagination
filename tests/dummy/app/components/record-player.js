d2f338bfa7114fc713f5accf3916cd703732ff61
Alex Ford <alexford@frontside.io>
2015-11-24 15:43:40 -0600

    Fix left and right horizon in demo

--- tests/dummy/app/components/record-player.js
+++ tests/dummy/app/components/record-player.js
@@ -9,13 +9,13 @@ export default Ember.Component.extend({
   'datasetState': null,
   elementWidth: null,
   totalRecords: Ember.computed.readOnly('records.length'),
-  pageWidth: Ember.computed('incrementWidth', 'pages.firstObject', function(){
+  pageWidth: Ember.computed('incrementWidth', 'pages.firstObject.size', function(){
     return this.get('incrementWidth') * this.get('pages.firstObject.size');
   }),
   incrementWidth: Ember.computed('elementWidth', 'totalRecords', function(){
     return this.get('elementWidth') / this.get('totalRecords');
   }),
-  readHeadOffset: Ember.computed('records.readOffset', 'incrementWidth', 'pageWidth', function(){
+  readHeadOffset: Ember.computed('records.readOffset', 'incrementWidth', function(){
     let incrementWidth = this.get('incrementWidth');
     return this.get('records.readOffset') * incrementWidth;
   }),
@@ -25,18 +25,18 @@ export default Ember.Component.extend({
   readHeadStyle: Ember.computed('readHeadOffset', function(){
     return Ember.String.htmlSafe("left:"+this.get('readHeadOffset')+"px;");
   }),
-  loadHorizonStyle: Ember.computed('loadHorizon', 'pageWidth', 'readHeadOffset', 'pages.firstObject', function(){
-    let left = this.get('readHeadOffset') - (this.get('loadHorizon')*this.get('pageWidth')) / this.get('pages.firstObject.size');
-    let width = 2*this.get('loadHorizon')*this.get('pageWidth') / this.get('pages.firstObject.size');
+  loadHorizonStyle: Ember.computed('loadHorizon', 'incrementWidth', 'readHeadOffset', 'pages.firstObject', function(){
+    let left = this.get('readHeadOffset') - (this.get('loadHorizon') * this.get('incrementWidth'));
+    let width = 2 * this.get('loadHorizon') * this.get('incrementWidth');
 
     return Ember.String.htmlSafe("left:"+left+"px; width:"+width+"px;");
   }),
-  unloadHorizonLeftStyle: Ember.computed('elementWidth', 'unloadHorizon', 'pageWidth', 'readHeadOffset', function(){
-    let right = this.get('elementWidth') - this.get('readHeadOffset') + (this.get('unloadHorizon')*this.get('pageWidth'));
+  unloadHorizonLeftStyle: Ember.computed('elementWidth', 'unloadHorizon', 'incrementWidth', 'readHeadOffset', function(){
+    let right = this.get('elementWidth') - this.get('readHeadOffset') + (this.get('unloadHorizon')*this.get('incrementWidth'));
     return Ember.String.htmlSafe("right:"+right+"px;");
   }),
-  unloadHorizonRightStyle: Ember.computed('unloadHorizon', 'pageWidth', 'readHeadOffset', function(){
-    let left = this.get('readHeadOffset') + (this.get('unloadHorizon')*this.get('pageWidth'));
+  unloadHorizonRightStyle: Ember.computed('unloadHorizon', 'incrementWidth', 'readHeadOffset', function(){
+    let left = this.get('readHeadOffset') + (this.get('unloadHorizon')*this.get('incrementWidth'));
     return Ember.String.htmlSafe("left:"+left+"px;");
   }),
   _adjustWidth(){
