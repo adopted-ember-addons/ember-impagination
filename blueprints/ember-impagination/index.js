f60d0035a3bdb528574e596f65a71719553ddcbc
Alex Ford <alexford@frontside.io>
2016-11-23 15:21:28 -0600

    Add Installation blueprint for ember-impagiantion

---
+++ blueprints/ember-impagination/index.js
@@ -0,0 +1,17 @@
+/*jshint node:true*/
+module.exports = {
+  description: 'Installation blueprint for ember-impagiantion',
+  normalizeEntityName: function() {},
+
+  afterInstall: function() {
+    return this.addAddonsToProject({
+      packages: [
+        {name: 'ember-browserify', target: '^1.1.13'},
+      ]
+    }).then(function() {
+      return this.addPackagesToProject([
+        {name: 'impagination', target: "^1.0.0-alpha.2"}
+      ]);
+    }.bind(this));
+  }
+};
